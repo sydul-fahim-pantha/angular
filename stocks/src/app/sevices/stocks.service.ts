78ab3361685a7fd05bee2fad5dbeeb133c2d501f
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-11-09 08:18:12 +0600

    ng generate service services/stocks

--- stocks/src/app/sevices/stocks.service.ts
+++ stocks/src/app/sevices/stocks.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service = 'https://angular2-in-action-api.herokuapp.com';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
+
 @Injectable()
 export class StocksService {
-
-  constructor() { }
-
+  constructor() {}
 }
