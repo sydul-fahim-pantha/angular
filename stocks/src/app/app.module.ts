31c6905e2e69cb53e607e410a649de11465e61e0
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-11-09 08:33:14 +0600

    ng generate component components/summary

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
